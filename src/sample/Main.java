5d88afa4df81e372f6abcaaa22bea314289b567e
Jens Christensen <paviln@outlook.dk>
2020-01-07 20:54:23 +0100

    fxml primary stage

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -9,13 +9,15 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception
+    {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Scene scene = new Scene(root, 600, 500);
+        primaryStage.setTitle("MediaPlayer");
+        // primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("hospital.png")));
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
