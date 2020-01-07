fb9fdd04144a8d120c1f7f289ca6659b8e5d750f
Islendingurinn <arnibent1312@hotmail.com>
2020-01-07 21:48:32 +0100

    Backend

--- src/code/Main.java
+++ src/code/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package code;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception
     {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/display/sample.fxml"));
         Scene scene = new Scene(root, 600, 500);
         primaryStage.setTitle("MediaPlayer");
         // primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("hospital.png")));
