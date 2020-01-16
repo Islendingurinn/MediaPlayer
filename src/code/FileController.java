1381ab8a3d92fdaa95ec0bf3a768985479e3c986
Jens Christensen <paviln@outlook.dk>
2020-01-17 00:08:33 +0100

    Improved file handling

--- src/code/FileController.java
+++ src/code/FileController.java
@@ -22,28 +22,50 @@ public class FileController
 
     String path = "";
 
-    @FXML
     public static void init(Stage newStage)
     {
         stage = newStage;
     }
     @FXML
-    private void openFIle()
+    private void openFile()
     {
         FileChooser fileChooser = new FileChooser();
         FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("mp4 files (*.mp4)", "*.mp4");
         fileChooser.getExtensionFilters().add(extFilter);
         File file = fileChooser.showOpenDialog(stage);
-        path = file.toString();
-        fileInput.setText(path);
+        if(file == null)
+        {
+            fileInput.clear();
+        }
+        else
+        {
+            path = file.toString();
+            fileInput.setText(path);
+        }
     }
     @FXML
     private void save()
     {
-        Video video = new Video(-1, name.getText(), path, category.getText());
-        video.save();
-        Controller._DISPLAYEDVIDEOS.add(video.toString());
-        Controller._VIDEOS.add(video);
+        File file = new File(fileInput.getText());
+        String filetype = file.toString();
+        filetype = filetype.substring(filetype.lastIndexOf(".") + 1, filetype.length());
+
+        if(file.isFile() && filetype.equalsIgnoreCase("mp4") && !name.getText().isEmpty() && !category.getText().isEmpty())
+        {
+            Video video = new Video(-1, name.getText(), path, category.getText());
+            video.save();
+            Controller._DISPLAYEDVIDEOS.add(video.toString());
+            Controller._VIDEOS.add(video);
+            stage.hide();
+        }
+        else
+        {
+            fileInput.clear();
+        }
+    }
+    @FXML
+    private void close()
+    {
         stage.hide();
     }
 }
